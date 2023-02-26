d6072879d612a3979885f10e5738e4647ba82097
Harshal Jaiswal <94278515+HarshalJaiswalNEU@users.noreply.github.com>
2023-02-26 13:05:09 -0500

    Time: 1164 ms, Memory: 43.2 MB - LeetHub

---
+++ license-key-formatting/license-key-formatting.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public string LicenseKeyFormatting(string s, int k) {
+
+        String res = s.Replace("-", "");
+        if( res == ""){
+            return "";
+        }
+        StringBuilder builder = new StringBuilder();
+
+        int i = 1;
+        res = res.ToUpper();
+
+        for( int j = res.Length-1; j>=0  ; j--){
+            builder.Insert(0, res[j]);
+            if( i == k){
+                builder.Insert(0,"-");
+                i = 0;
+            }
+            i++;
+        }
+        // Console.WriteLine(builder.ToString());
+        if(builder[0] == '-'){
+            return builder.ToString(1, builder.Length-1);
+        }
+        
+
+
+        return builder.ToString();
+    }
+}
